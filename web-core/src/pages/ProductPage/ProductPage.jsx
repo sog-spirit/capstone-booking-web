05b5df2ae5058ebd346905ed8bd146896febc4c6
spirit <tbthanh2001@gmail.com>
2024-10-25 20:52:10 +0700

    Edit product feature added

--- web-core/src/pages/ProductPage/ProductPage.jsx
+++ web-core/src/pages/ProductPage/ProductPage.jsx
@@ -15,7 +15,7 @@ export default function ProductPage() {
     const [addNewModalState, setAddNewModalState] = useState(false);
     const [addNewFormData, setAddNewFormData] = useState({
         name: '',
-        price: '',
+        price: 0,
     });
     const [addNewInputState, setAddNewInputState] = useState({
         name: '',
@@ -23,6 +23,17 @@ export default function ProductPage() {
     });
 
     const [productList, setProductList] = useState([]);
+
+    const [editModalState, setEditModalState] = useState(false);
+    const [editFormData, setEditFormData] = useState({
+        id: 0,
+        name: '',
+        price: 0,
+    });
+    const [editInputState, setEditInputState] = useState({
+        name: '',
+        price: '',
+    });
 
     useEffect(() => {
         loadProductList();
@@ -47,12 +58,12 @@ export default function ProductPage() {
             setAddNewFormData(prevState => ({
                 ...prevState,
                 name: '',
-                address: '',
+                price: '',
             }));
             setAddNewInputState(prevState => ({
                 ...prevState,
                 name: '',
-                address: '',
+                price: '',
             }));
         }
     }
@@ -72,6 +83,58 @@ export default function ProductPage() {
         if (response.status === HTTP_STATUS.OK) {
             let productList = await response.json();
             setProductList(productList);
+        }
+    }
+
+    function openEditModal(prodcutId) {
+        let product = productList.find(item => item.id === prodcutId);
+        setEditFormData(prevState => ({
+            ...prevState,
+            id: prodcutId,
+            name: product.name,
+            price: product.price,
+        }));
+        setEditModalState(true);
+    }
+
+    function closeEditModal() {
+        setEditFormData(prevState => ({
+            ...prevState,
+            id: 0,
+            name: '',
+            price: 0,
+        }));
+        setEditModalState(false);
+    }
+
+    async function submitEditData() {
+        await refreshAccessToken(setTokenState);
+
+        const headers = new Headers();
+        headers.append(HTTP_REQUEST_HEADER_NAME.CONTENT_TYPE, HTTP_REQUEST_HEADER_VALUE.APPLICATION_JSON);
+        headers.append(HTTP_REQUEST_HEADER_NAME.AUTHORIZATION, tokenState.accessToken);
+
+        const response = await fetch(BASE_API_URL + PRODUCT_URL.BASE, {
+            method: HTTP_REQUEST_METHOD.PUT,
+            headers: headers,
+            body: JSON.stringify(editFormData),
+        });
+
+        if (response.status === HTTP_STATUS.OK) {
+            setEditModalState(false);
+            defaultSuccessToastNotification(MESSAGE_CONSTS.EDIT_SUCCESS);
+            setEditFormData(prevState => ({
+                ...prevState,
+                id: 0,
+                name: '',
+                price: 0,
+            }));
+            setEditInputState(prevState => ({
+                ...prevState,
+                name: '',
+                price: '',
+            }));
+            loadProductList();
         }
     }
 
@@ -118,6 +181,11 @@ export default function ProductPage() {
                                     Price: {item.price}
                                 </div>
                             </div>
+                            <div className="product-page__container__product-list__list__item__button-group">
+                                <div className="product-page__container__product-list__list__item__button-group__edit-button" onClick={() => openEditModal(item.id)}>
+                                    Edit
+                                </div>
+                            </div>
                         </div>
                         ))}
                     </div>
@@ -148,6 +216,35 @@ export default function ProductPage() {
                     <div className="product-page__add-new-modal__form__footer">
                         <div className="product-page__add-new-modal__form__footer__add-button" onClick={() => submitAddNewProduct()}>
                             Add
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="product-page__edit-modal" style={editModalState ? {} : {display: 'none'}}>
+                <div className="product-page__edit-modal__form">
+                    <div className="product-page__edit-modal__form__header">
+                        <div className="product-page__edit-modal__form__header__title">
+                            <h5>Edit product</h5>
+                        </div>
+                        <div className="product-page__edit-modal__form__header__close-button" onClick={() => closeEditModal()}>
+                            Close
+                        </div>
+                    </div>
+                    <div className="product-page__edit-modal__form__content">
+                        <div className="product-page__edit-modal__form__content__name">
+                            <div className="product-page__edit-modal__form__content__name__label">Name</div>
+                            <input type="text" placeholder="Name" name="name" value={editFormData.name} onChange={event => handleInputChange(event, setEditFormData)} className={`product-page__edit-modal__form__content__name__input ${editInputState.name ? 'input-error' : ''}`} />
+                            <div className="product-page__edit-modal__form__content__name__error-message input-error-message">{editInputState.name ? editInputState.name : ''}</div>
+                        </div>
+                        <div className="product-page__edit-modal__form__content__price">
+                            <div className="product-page__edit-modal__form__content__price__label">Price</div>
+                            <input type="text" placeholder="Price" name="price" value={editFormData.price} onChange={event => handleInputChange(event, setEditFormData)} className={`product-page__edit-modal__form__content__price__input ${editInputState.price ? 'input-error' : ''}`} />
+                            <div className="product-page__edit-modal__form__content__price__error-message input-error-message">{editInputState.price ? editInputState.price : ''}</div>
+                        </div>
+                    </div>
+                    <div className="product-page__edit-modal__form__footer">
+                        <div className="product-page__edit-modal__form__footer__save-button" onClick={() => submitEditData()}>
+                            Save
                         </div>
                     </div>
                 </div>
