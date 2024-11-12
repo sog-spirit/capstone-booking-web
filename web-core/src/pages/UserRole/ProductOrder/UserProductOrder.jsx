3ec3b7aa0f69dede6207a7dbab2a140fe7e03e11
sog-spirit <78286856+sog-spirit@users.noreply.github.com>
2024-11-12 16:53:34 +0700

    Missing product inventory quantity attributes created

--- web-core/src/pages/UserRole/ProductOrder/UserProductOrder.jsx
+++ web-core/src/pages/UserRole/ProductOrder/UserProductOrder.jsx
@@ -43,26 +43,24 @@ export default function UserProductOrder() {
         }
     }
 
-    function addProductToCart(productId) {
-        let cartIem = cartList.find(item => item.productId === productId);
+    function addProductToCart(productInventoryId) {
+        let cartIem = cartList.find(item => item.id === productInventoryId);
         if (cartIem) {
-            addQuantity(productId);
+            addQuantity(productInventoryId);
         } else {
-            let product = productList.find(item => item.productId === productId);
-            let newCartIem = {
-                productId: product.productId,
-                productName: product.productName,
-                productPrice: product.productPrice,
+            let productInventoryItem = productList.find(item => item.id === productInventoryId);
+            let newCartItem = {
+                ...productInventoryItem,
                 quantity: 1,
             };
-            setCartList(prevState => [...prevState, newCartIem]);
-            setCartPriceTotal(prevState => prevState + product.productPrice);
+            setCartList(prevState => [...prevState, newCartItem]);
+            setCartPriceTotal(prevState => prevState + newCartItem.productPrice);
         }
     }
 
-    function addQuantity(productId) {
+    function addQuantity(productInventoryId) {
         setCartList(cartList.map(item => {
-            if (item.productId === productId) {
+            if (item.id === productInventoryId) {
                 setCartPriceTotal(prevState => prevState + item.productPrice);
                 return {
                     ...item,
@@ -74,9 +72,9 @@ export default function UserProductOrder() {
         }).filter(item => item.quantity > 0));
     }
 
-    function subtractQuantity(productId) {
+    function subtractQuantity(productInventoryId) {
         setCartList(cartList.map(item => {
-            if (item.productId === productId) {
+            if (item.id === productInventoryId) {
                 setCartPriceTotal(prevState => prevState - item.productPrice);
                 return {
                     ...item,
@@ -88,9 +86,9 @@ export default function UserProductOrder() {
         }).filter(item => item.quantity > 0));
     }
 
-    function removeCartIem(productId) {
+    function removeCartIem(productInventoryId) {
         setCartList(cartList.filter(item => {
-            if (item.productId !== productId) {
+            if (item.id !== productInventoryId) {
                 return true;
             } else {
                 setCartPriceTotal(prevState => prevState - (item.productPrice * item.quantity));
@@ -117,7 +115,7 @@ export default function UserProductOrder() {
 
         let cartItemList = cartList.map(item => {
             return {
-                productId: item.productId,
+                productInventoryId: item.id,
                 quantity: item.quantity,
             };
         });
@@ -164,7 +162,7 @@ export default function UserProductOrder() {
                     </div>
                     <div className="user-product-order__container__product-list__list">
                         {productList.map(item => (
-                        <div className="user-product-order__container__product-list__list__item" key={item.productId}>
+                        <div className="user-product-order__container__product-list__list__item" key={item.id}>
                             <div className="user-product-order__container__product-list__list__item__info">
                                 <div className="user-product-order__container__product-list__list__item__info__img">
                                     <img src={API_URL.BASE + API_URL.IMAGE.BASE + API_URL.IMAGE.PRODUCT + `?productId=${item.productId}`} />
@@ -177,9 +175,12 @@ export default function UserProductOrder() {
                                 <div className="user-product-order__container__product-list__list__item__detail__price">
                                     Price: {item.productPrice}
                                 </div>
+                                <div className="user-product-order__container__product-list__list__list__item__detail__quantity">
+                                    Quantity: {item.quantity}
+                                </div>
                             </div>
                             <div className="user-product-order__container__product-list__list__item__button-group">
-                                <div className="user-product-order__container__product-list__list__item__button-group__add-to-cart-button" onClick={() => addProductToCart(item.productId)}>
+                                <div className="user-product-order__container__product-list__list__item__button-group__add-to-cart-button" onClick={() => addProductToCart(item.id)}>
                                     Add to cart
                                 </div>
                             </div>
@@ -200,7 +201,7 @@ export default function UserProductOrder() {
                     </div>
                     <div className="user-product-order__cart-modal__form__cart-detail">
                         {cartList.map(item => (
-                        <div className="user-product-order__cart-modal__form__cart-detail__item" key={item.productId}>
+                        <div className="user-product-order__cart-modal__form__cart-detail__item" key={item.id}>
                             <div className="user-product-order__cart-modal__form__cart-detail__item__detail">
                                 <div className="user-product-order__cart-modal__form__cart-detail__item__detail__img">
                                 <img src={API_URL.BASE + API_URL.IMAGE.BASE + API_URL.IMAGE.PRODUCT + `?productId=${item.productId}`} />
@@ -216,18 +217,18 @@ export default function UserProductOrder() {
                             </div>
                             <div className="user-product-order__cart-modal__form__cart-detail__item__button-group">
                                 <div className="user-product-order__cart-modal__form__cart-detail__item__button-group__first">
-                                    <div className="user-product-order__cart-modal__form__cart-detail__item__button-group__first__remove-button" onClick={() => removeCartIem(item.productId)}>
+                                    <div className="user-product-order__cart-modal__form__cart-detail__item__button-group__first__remove-button" onClick={() => removeCartIem(item.id)}>
                                         X
                                     </div>
                                 </div>
                                 <div className="user-product-order__cart-modal__form__cart-detail__item__button-group__quantity">
-                                    <div className="user-product-order__cart-modal__form__cart-detail__item__button-group__quantity__subtract-button" onClick={() => subtractQuantity(item.productId)}>
+                                    <div className="user-product-order__cart-modal__form__cart-detail__item__button-group__quantity__subtract-button" onClick={() => subtractQuantity(item.id)}>
                                         -
                                     </div>
                                     <div className="user-product-order__cart-modal__form__cart-detail__item__button-group__quantity__label">
                                         {item.quantity}
                                     </div>
-                                    <div className="user-product-order__cart-modal__form__cart-detail__item__button-group__quantity__add-button" onClick={() => addQuantity(item.productId)}>
+                                    <div className="user-product-order__cart-modal__form__cart-detail__item__button-group__quantity__add-button" onClick={() => addQuantity(item.id)}>
                                         +
                                     </div>
                                 </div>
